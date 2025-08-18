c019b9afc53fa793bb517dcc0e362b6ccaa2a4f7
rrishabh009 <trishabh280@gmail.com>
2025-08-18 23:35:00 +0530

    18 agst commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,8 +41,8 @@ const Navbar = () => {
     return (
         
            
-            <nav className={`fixed top-0 left-0 w-full flex items-center justify-between px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 ${isScrolled ? "bg-white/80 shadow-md text-gray-700 backdrop-blur-lg py-3 md:py-4" : "bg-black/50 text-white py-4 md:py-6"}`}>
-
+            <nav className={`fixed top-0 left-0 w-full flex items-center justify-between px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 ${isScrolled ? "bg-white/80 shadow-md text-gray-700 backdrop-blur-lg py-3 md:py-4" : "py-4 md:py-6"}`}>
+    
                 {/* Logo */}
                 <Link to='/'>
                     <img src={assets.logo} alt="logo" className={`h-9 ${isScrolled && "invert opacity-80"}`} />
